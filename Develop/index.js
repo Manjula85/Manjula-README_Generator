59d04225d5c9d6f2b204915b7bb166ae45121967
Manjula Guneratne <uftcodingbootcamp@gmail.com>
2021-02-08 04:46:42 -0500

    impass v1.0

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,46 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generatePage = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
 const questions = [];
 
+questions[0] = 'Enter project title here: ';
+questions[1] = 'Enter desciption: ';
+questions[2] = 'Installation instructions: ';
+questions[3] = 'Usage information: ';
+questions[4] = 'Contribution guidelines: ';
+questions[5] = 'Test instructions: ';
+questions[6] = 'Choose a license from below'; //Give options
+questions[7] = 'Enter GitHub username: ';
+questions[8] = 'Enter email address: ';
+//Create a table of contents with links to connect to where there are
+
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    generateMarkdown();
+};
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: questions[0],
+            validate: titleInput => {
+                if (titleInput) {
+                    return false;
+                } else {
+                    console.log('Please enter the title');
+                    return false;
+                }
+            }
+        }
+    ])
+    .then(test => console.log(test));
+};
 
 // Function call to initialize app
-init();
+init(); 
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -14,7 +14,12 @@ function renderLicenseSection(license) {}
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+  A diam sollicitudin tempor id eu. Porttitor massa id neque aliquam vestibulum morbi blandit cursus risus. Feugiat nisl pretium fusce id. Etiam erat velit scelerisque in dictum non. Odio euismod lacinia at quis. Consectetur adipiscing elit ut aliquam purus sit amet luctus. Eu ultrices vitae auctor eu augue ut lectus arcu bibendum. Purus faucibus ornare suspendisse sed nisi lacus sed viverra. Consectetur adipiscing elit pellentesque habitant morbi tristique senectus. Dictum non consectetur a erat. Enim ut sem viverra aliquet eget sit amet. Nisi vitae suscipit tellus mauris a diam. Tellus at urna condimentum mattis pellentesque id. Odio tempor orci dapibus ultrices in iaculis nunc sed augue. 
+  Felis donec et odio pellentesque diam volutpat. Amet risus nullam eget felis. Tempor nec feugiat nisl pretium.
+
+  //This is where the Readme is created!!!
 `;
-}
+};
 
 module.exports = generateMarkdown;
